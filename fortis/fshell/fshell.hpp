c50aa3d394759dd0beb66a8c829c6c156651c17b
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-30 17:55:18 -0500

    Add initial FShell implementation with empty repl function
    Initial implementation of FShell with a placeholder repl function.

---
+++ fortis/fshell/fshell.hpp
@@ -0,0 +1,18 @@
+/*
+FShell
+Written in C++17 starting on 3-30-26
+Adam Tillman
+
+Compiles correctly on:
+
+C++14
+C++17
+C++20
+C++23
+
+The code is so short because the kernel is essentially the shell's compiler.
+In summary, this is just a REPL for kernel bytecode. 
+*/
+#include "kernel.hpp"
+// I made this at school and ran out of time, so for now repl() is empty.
+void repl() {}
