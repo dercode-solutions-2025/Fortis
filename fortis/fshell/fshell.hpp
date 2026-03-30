eeae8d892796d30660eda4e14c7385c348b8f97d
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-30 17:56:00 -0500

    Fix include path for kernel.hpp in fshell.hpp
    Updated include path for kernel.hpp to reflect new directory structure.

--- fortis/fshell/fshell.hpp
+++ fortis/fshell/fshell.hpp
@@ -13,6 +13,6 @@ C++23
 The code is so short because the kernel is essentially the shell's compiler.
 In summary, this is just a REPL for kernel bytecode. 
 */
-#include "../kernel.hpp"
+#include "../../kernel.hpp"
 // I made this at school and ran out of time, so for now repl() is empty.
 void repl() {}
